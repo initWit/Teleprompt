6c568b4969020e7e0590f3c0561ee6f47a1605a5
Robert Figueras <RPF_MacBookAir@Roberts-MacBook-Air.local>
2014-05-02 11:13:54 -0500

    Initial Commit

---
+++ Teleprompt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Teleprompt
+//
+//  Created by Robert Figueras on 5/2/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Teleprompt/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Teleprompt
+//
+//  Created by Robert Figueras on 5/2/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
